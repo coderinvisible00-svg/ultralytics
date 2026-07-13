421a56fc7c1515f872ed9374fc2fbeaa004cb89d
coderinvisible00-svg <coderinvisible00@gmail.com>
2026-07-14 04:41:35 +0600

    Update CoordAtt initialization with additional parameter
    Signed-off-by: coderinvisible00-svg <coderinvisible00@gmail.com>

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -339,7 +339,7 @@ class C2fCA(nn.Module):
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
-        self.ca = CoordAtt(c2)
+        self.ca = CoordAtt(c2,c2)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass through C2f layer."""
