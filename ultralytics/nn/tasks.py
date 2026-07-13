c3b37533bddf82059933ef2d486027649a02d98c
coderinvisible00-svg <coderinvisible00@gmail.com>
2026-07-14 03:58:22 +0600

    Update tasks.py
    Signed-off-by: coderinvisible00-svg <coderinvisible00@gmail.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -10,6 +10,8 @@ from pathlib import Path
 import torch
 import torch.nn as nn
 
+from ultralytics.nn.modules import C2fCA
+
 from ultralytics.nn.autobackend import check_class_names
 from ultralytics.nn.modules import (
     AIFI,
